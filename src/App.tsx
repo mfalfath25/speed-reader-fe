734a65644e977f54a6d930872f5a1337a2fdc88a
Muhammad Fajar Alfath <mfalfath25@gmail.com>
2022-10-22 10:31:18 +0700

    chore: setup vite-react-ts & dependencies

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
+      </div>
+      <h1 className="text-3xl font-bold">Tailwind Setup</h1>
+    </div>
+  )
+}
+
+export default App
