fc7958599dcd210b74fa0d615af1f0db5bdde650
Fajar Alfath <mfalfath25@gmail.com>
2023-01-15 09:46:38 +0700

    fix: FormSettingsValues import

--- src/api/mutation/useEditSettingMutation.tsx
+++ src/api/mutation/useEditSettingMutation.tsx
@@ -1,11 +1,11 @@
 import { baseAPI } from '../utils'
 import { useMutation } from '@tanstack/react-query'
-import { SettingsValues } from '../../types/model'
+import { FormSettingsValues } from '../../types/model'
 import { useUserStore } from '../../stores'
 
 export const useEditSettingMutation = () => {
   const { userData } = useUserStore()
-  const mutation = useMutation((data: SettingsValues) =>
+  const mutation = useMutation((data: FormSettingsValues) =>
     baseAPI.put(`/setting/${userData.userId}`, data)
   )
 
