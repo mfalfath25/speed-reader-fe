3668882392bc499e7575f6bbccb2bebab46233be
Fajar Alfath <mfalfath25@gmail.com>
2023-02-15 21:24:31 +0700

    feat: add vite-plugin-pwa

--- src/components/molecules/trainingSimulations/ModeCustom.tsx
+++ src/components/molecules/trainingSimulations/ModeCustom.tsx
@@ -72,7 +72,7 @@ export const ModeCustom = () => {
               {trainingData.wpm}
             </label>
           ) : null}
-          <div className="relative max-h-[500px] w-full overflow-y-auto rounded-md bg-slate-100 p-0 outline outline-1 outline-offset-0">
+          <div className="scroll relative max-h-[500px] w-full overflow-y-auto rounded-md bg-slate-100 p-0 outline outline-1 outline-offset-0">
             <pre
               className="relative whitespace-pre-line p-2 text-left text-base font-normal sm:text-xl"
               style={{
